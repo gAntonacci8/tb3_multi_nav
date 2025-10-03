7e9d040c317d4bf1a5ad72248a65f87a1fa20e40
gAntonacci <josh@MSI.localdomain>
2025-10-03 23:53:02 +0200

    Fixed redundant .yaml extension

--- ros_ws/save_map.sh
+++ ros_ws/save_map.sh
@@ -1,1 +1,1 @@
-ros2 run nav2_map_server map_saver_cli -f /root/ros_ws/src/tb3_multi_nav/utils/simple_maze.yaml
+ros2 run nav2_map_server map_saver_cli -f /root/ros_ws/src/tb3_multi_nav/utils/simple_maze
