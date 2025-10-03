d79b48e6b0f0149a623a4e0fd3658605488a2d97
gAntonacci <josh@MSI.localdomain>
2025-10-03 21:16:01 +0200

    TB3 model corrected.

--- docker_ws/ros_entrypoint.sh
+++ docker_ws/ros_entrypoint.sh
@@ -4,5 +4,5 @@ source /opt/ros/jazzy/setup.bash
 if [ -f "/root/ros_ws/install/setup.bash" ]; then
   source /root/ros_ws/install/setup.bash
 fi
-export TURTLEBOT3_MODEL=burger_cam
+export TURTLEBOT3_MODEL=waffle
 exec "$@"
