4f23c3903797a09a5cc1455f266dd10e647292e7
gAntonacci8 <g.antonacci8@studenti.poliba.it>
2025-11-18 17:29:08 +0100

    Delete ros_ws/build directory

--- ros_ws/build/turtlebot3_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/ros_ws/install/turtlebot3_description/share/turtlebot3_description/package.sh"
-. "/root/ros_ws/install/turtlebot3_node/share/turtlebot3_node/package.sh"
